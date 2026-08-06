1f6c10b971dc4cca21565517ca6f487bb1205583
ShaikhSalman0808 <ShaikhSalman0808@users.noreply.github.com>
2026-08-06 19:38:10 +0530

    Update feature flag evaluation and group_targeting rules and usergroupmembership

--- app/services/evaluation_engine.py
+++ app/services/evaluation_engine.py
@@ -1,13 +1,15 @@
 from sqlalchemy.orm import Session
 from app.models.environment import Environment
 from app.models.flag import Flag
+from app.models.targeting_rule import TargetingRule
+from app.models.user_group_membership import UserGroupMembership
 
 
 def evaluate_flag(
-db: Session,
-flag_key: str,
-environment_name: str,
-user_context: dict | None = None
+    db: Session,
+    flag_key: str,
+    environment_name: str,
+    user_context: dict | None = None
 ):
     environment = (
         db.query(Environment)
@@ -32,6 +34,61 @@ user_context: dict | None = None
             "success": False,
             "message": "Feature flag not found"
         }
+
+    if user_context:
+        user_id = str(user_context.get("user_id"))
+
+        rule = (
+            db.query(TargetingRule).filter(
+                TargetingRule.flag_id == flag.id,
+                TargetingRule.attribute == "user_id",
+                TargetingRule.operator == "=",
+                TargetingRule.value == user_id
+            ).first()
+        )
+
+        if rule:
+            return {
+                "success": True,
+                "message": "Matched User Targeting Rule",
+                "environment": environment.name,
+                "flag": flag.key,
+                "type": flag.type,
+                "enabled": flag.enabled,
+                "value": flag.default_value,
+                "user_context": user_context
+            }
+
+        group_membership = (
+            db.query(UserGroupMembership)
+            .filter(UserGroupMembership.user_id == user_id)
+            .first()
+        )
+
+        if group_membership:
+            group_rule = (
+                db.query(TargetingRule)
+                .filter(
+                    TargetingRule.flag_id == flag.id,
+                    TargetingRule.attribute.in_(["group_name", "group_id"]),
+                    TargetingRule.operator == "=",
+                    TargetingRule.value == group_membership.group_name
+                )
+                .first()
+            )
+
+            if group_rule:
+                return {
+                    "success": True,
+                    "message": "Matched Group Targeting Rule",
+                    "environment": environment.name,
+                    "flag": flag.key,
+                    "type": flag.type,
+                    "enabled": flag.enabled,
+                    "value": flag.default_value,
+                    "user_context": user_context
+                }
+
     return {
         "success": True,
         "environment": environment.name,
@@ -41,3 +98,4 @@ user_context: dict | None = None
         "value": flag.default_value,
         "user_context": user_context
     }
+
--- tests/test_engine.py
+++ tests/test_engine.py
@@ -4,6 +4,10 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from app.database.session import SessionLocal
+from app.models.environment import Environment
+from app.models.flag import Flag
+from app.models.targeting_rule import TargetingRule
+from app.models.user_group_membership import UserGroupMembership
 from app.services.evaluation_engine import evaluate_flag
 
 
@@ -23,8 +27,7 @@ def test_default_value_fallback():
     print(result)
     db.close()
     assert result["success"] is True
-    assert result["enabled"] == True  # Replace with the expected default value for the flag
-
+    assert result["enabled"] == True  
 def test_disabled_flag():
 
     db = SessionLocal()
@@ -77,6 +80,84 @@ def test_empty_user_context():
     assert result["enabled"] is True
 
 
+def test_user_targeting():
+
+    db = SessionLocal()
+    try:
+        env = db.query(Environment).filter(Environment.name == "development").first()
+        flag = db.query(Flag).filter(Flag.key == "dark_mode", Flag.environment_id == env.id).first()
+
+        rule = db.query(TargetingRule).filter(TargetingRule.flag_id == flag.id, TargetingRule.attribute == "user_id").first()
+        if not rule:
+            rule = TargetingRule(flag_id=flag.id, attribute="user_id", operator="=", value="101")
+            db.add(rule)
+            db.commit()
+            db.refresh(rule)
+        else:
+            rule.operator = "="
+            rule.value = "101"
+            db.commit()
+
+        result = evaluate_flag(
+            db=db,
+            flag_key="dark_mode",
+            environment_name="development",
+            user_context={
+                "user_id": "101"
+            }
+        )
+        print("Targeting match result:", result)
+        assert result["success"] is True
+        assert result["message"] == "Matched User Targeting Rule"
+    finally:
+        db.close()
+
+def test_user_group_targeting():
+
+    db = SessionLocal()
+    try:
+        env = db.query(Environment).filter(Environment.name == "development").first()
+        flag = db.query(Flag).filter(Flag.key == "dark_mode", Flag.environment_id == env.id).first()
+
+        membership = db.query(UserGroupMembership).filter(UserGroupMembership.user_id == "102").first()
+        if not membership:
+            membership = UserGroupMembership(user_id="102", group_name="admin")
+            db.add(membership)
+            db.commit()
+            db.refresh(membership)
+        else:
+            membership.group_name = "admin"
+            db.commit()
+
+        rule = db.query(TargetingRule).filter(TargetingRule.flag_id == flag.id, TargetingRule.attribute == "group_name").first()
+        if not rule:
+            rule = TargetingRule(flag_id=flag.id, attribute="group_name", operator="=", value="admin")
+            db.add(rule)
+            db.commit()
+            db.refresh(rule)
+        else:
+            rule.attribute = "group_name"
+            rule.operator = "="
+            rule.value = "admin"
+            db.commit()
+
+        result = evaluate_flag(
+            db=db,
+            flag_key="dark_mode",
+            environment_name="development",
+            user_context={
+                "user_id": "102",
+                "groups": ["admin", "team1"],
+                "country": "US"
+            }
+        )
+        print("Group targeting match result:", result)
+        assert result["success"] is True
+        assert result["message"] == "Matched Group Targeting Rule"
+    finally:
+        db.close()
+
+
 if __name__ == "__main__":
     print("--- Running test_default_value_fallback ---")
     test_default_value_fallback()
@@ -86,4 +167,8 @@ if __name__ == "__main__":
     test_envirnment_override()
     print("--- Running test_empty_user_context ---")
     test_empty_user_context()
+    print("--- Running test_user_targeting ---")
+    test_user_targeting()
+    print("--- Running test_user_group_targeting ---")
+    test_user_group_targeting()
     print("\nAll test_engine.py tests completed successfully!")
